fc962731d9fadd6714c74ebf0a4fa620be63f4b2
immeasurable-vikrant <vikrant.immeasurable@gmail.com>
2019-10-30 15:56:26 +0530

    Refactoring modules

--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,17 @@ passport.use(
        callbackURL: '/auth/google/callback',
        proxy: true
       },
-      (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId:profile.id }).then((existingUser)=> {
-          if(existingUser) {
-            //we already have record with the given provided Id.
-            done(null, existingUser);
-          } else {
-            // we don't have a user record with this ID, make a new record!
-            new User({ googleId: profile.id})
-            .save()
-            .then(user => done(null, user));
-          }
-        })     
+      async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId:profile.id })
+
+        if(existingUser) {
+          //we already have record with the given provided Id.
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record!
+        const user = await new User({ googleId: profile.id}).save()
+        done(null, user);
+        }   
       }
      )
    );
